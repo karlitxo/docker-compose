65ac48ece5473f406aebf1b5e1ad3eed5e67b130
karlitxo <karlitxo@gmail.com>
2019-07-01 13:01:26 +0100

    Config changes for mongoDB: amend

--- mongoDB/replica-set/build/entrypoint/docker-entrypoint.sh
+++ mongoDB/replica-set/build/entrypoint/docker-entrypoint.sh
@@ -7,9 +7,9 @@ rs.initiate(
   {
     _id : "$REPLICASET_NAME",
     members: [
-      { _id : 0, host : "primary:27017" },
-      { _id : 1, host : "secondary_01:27017" },
-      { _id : 2, host : "secondary_02:27017" }
+      { _id : 0, host : "$primary_IP:27017" },
+      { _id : 1, host : "$secondary_01_IP:27017" },
+      { _id : 2, host : "$secondary_02_IP:27017" }
     ]
   }
 )
